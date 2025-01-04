105b8ad2f34b088277391e95d952e73d4e52db2d
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2025-01-04 21:29:49 +0300

    formatting

--- main.c
+++ main.c
@@ -1,21 +1,17 @@
 #include<windows.h>
-#include <shellapi.h>
+#include<shellapi.h>
 
 int mainCRTStartup(void) { 
     LPWSTR *argv;
     int argc;
-
     argv = CommandLineToArgvW(GetCommandLineW(), &argc);
 
-    if (argc <= 1) {
-        return 0;
-    } else {
-        int last = 0;
-        while (!(argv[1][last] == 0)) {
-            last++;
-        }
-        return last;
-    }
+    if (argc <= 1) return 0;
+    
+    int last = 0;
+    while (argv[1][last] != 0) last++;
+    
+    return last;
 }
 
 int main(int argc, char** argv) { return 0; }
