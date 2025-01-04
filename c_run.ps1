6e759c24c243bc4fa5818710ab5830f5bbabea6c
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2025-01-04 20:49:58 +0300

    use clang for the entire compilation

--- c_run.ps1
+++ c_run.ps1
@@ -1,6 +1,6 @@
 clang -c ./C-Version/main.c -m32 -nostdlib -o main.o
 & "C:\Program Files\LLVM\bin\llvm-objcopy.exe" --redefine-sym _GetCommandLineA=_GetCommandLineA@0 main.o
-link "C:\Program Files (x86)\Windows Kits\10\lib\10.0.22000.0\um\x86\kernel32.lib" "C:\Program Files (x86)\Windows Kits\10\lib\10.0.22000.0\um\x86\shell32.lib" /OUT:main.exe main.o
+clang main.o "C:\Program Files (x86)\Windows Kits\10\lib\10.0.22000.0\um\x86\kernel32.lib" "C:\Program Files (x86)\Windows Kits\10\lib\10.0.22000.0\um\x86\shell32.lib" -o main.exe
 if ( $args.count -gt 0 ){
     (Start-Process ./main.exe $args[0] -PassThru -Wait).ExitCode
 } else {
