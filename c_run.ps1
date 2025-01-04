b65951671dc4df3529be8878c11fd4767ae0dbf0
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2025-01-04 21:05:10 +0300

    move main.c to project root

--- c_run.ps1
+++ c_run.ps1
@@ -2,7 +2,7 @@ $objcopy = "C:\Program Files\LLVM\bin\llvm-objcopy.exe"
 $kernel32libpath = "C:\Program Files (x86)\Windows Kits\10\lib\10.0.22000.0\um\x86\kernel32.lib"
 $shell32libpath = "C:\Program Files (x86)\Windows Kits\10\lib\10.0.22000.0\um\x86\shell32.lib"
 
-clang -c ./C-Version/main.c -m32 -nostdlib -o main.o
+clang -c main.c -m32 -nostdlib -o main.o
 & $objcopy --redefine-sym _GetCommandLineA=_GetCommandLineA@0 main.o
 clang main.o $kernel32libpath $shell32libpath -o main.exe
 if ( $args.count -gt 0 ){
