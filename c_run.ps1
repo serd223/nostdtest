70ce16885a9365261d8d512dfe653ab129f8bd24
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2025-01-04 20:52:36 +0300

    extract paths into variables in the c_run.ps1 script

--- c_run.ps1
+++ c_run.ps1
@@ -1,6 +1,10 @@
+$objcopy = "C:\Program Files\LLVM\bin\llvm-objcopy.exe"
+$kernel32libpath = "C:\Program Files (x86)\Windows Kits\10\lib\10.0.22000.0\um\x86\kernel32.lib"
+$shell32libpath = "C:\Program Files (x86)\Windows Kits\10\lib\10.0.22000.0\um\x86\shell32.lib"
+
 clang -c ./C-Version/main.c -m32 -nostdlib -o main.o
-& "C:\Program Files\LLVM\bin\llvm-objcopy.exe" --redefine-sym _GetCommandLineA=_GetCommandLineA@0 main.o
-clang main.o "C:\Program Files (x86)\Windows Kits\10\lib\10.0.22000.0\um\x86\kernel32.lib" "C:\Program Files (x86)\Windows Kits\10\lib\10.0.22000.0\um\x86\shell32.lib" -o main.exe
+& $objcopy --redefine-sym _GetCommandLineA=_GetCommandLineA@0 main.o
+clang main.o $kernel32libpath $shell32libpath -o main.exe
 if ( $args.count -gt 0 ){
     (Start-Process ./main.exe $args[0] -PassThru -Wait).ExitCode
 } else {
